8c36306916dad5774a104538329b34283c123742
Frontend <sweetysks23@gmail.com>
2023-11-06 18:44:58 +0900

    detailpage css완성

---
+++ src/components/DetailList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DetailList = () => {
+  return (
+    <div className="DetailSmallList">
+      <div className="DetailSmallImg">
+        <img src="/images/img-detail-small.jpg" alt="" />
+      </div>
+      <div className="DetailRtText">
+        <div>2023 ISU 피겨 그랑프리 3차_그랑프리 드 프랑스...</div>
+        <div className="DetailRtTextS">
+          스브스스포츠 SUBUSU SPORTS <br /> 조회수 1.7천회 23분 전
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailList;
